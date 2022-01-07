a854be4b0f183019fadb16afc6e227b062898627
Kyle Corga <82676455+KyleCorga@users.noreply.github.com>
2022-01-07 14:04:58 -0500

    Add files via upload

---
+++ SimDriver.py
@@ -0,0 +1,246 @@
+import random
+import xlwt
+from xlutils.copy import copy
+from xlrd import *
+from datetime import datetime
+from Zoob import Zoob
+
+#Global variables
+findFoodChallenge = 10#how much harder it becomes to find food each iteration of searching(can search a max of 10 rounds)
+#Arrays to store statistics of each iteration to print to a file
+fileNumAlive = []
+fileAltOne = []
+fileAltTwo = []
+fileAltThree = []
+fileMedianAlt = []
+fileAvgSocial = []
+fileAvgSpeed = []
+fileAvgSize = []
+fileAvgVision = []
+fileAvgAggr = []
+fileAvgFoodReq = []
+fileAvgTimeAlive = []
+    
+def main():
+    #variables
+    iterations = 1000#  how many iterations to run the simulation
+    zoobNumber = 50#    how many zoobs the simulation starts with
+    envFood = 200#   how much food the enviornment starts with
+    zoobList = generateZoobArray(zoobNumber,True)#array of randomized zoobsalso accepts maybe shared ancestor?
+    firstIteration = True
+    
+    #To write to file
+    wb = xlwt.Workbook()
+    sheetName = str('Z_'+str(zoobNumber)+'_I_'+str(iterations)+'_F_'+str(envFood))
+    if(len(sheetName)>20):
+        sheetName="Simulation Sheet"
+    ws = wb.add_sheet(str(sheetName))
+    
+    for i in range(iterations+1):
+        #####BEGINNING OF SIM ITERATION
+        if firstIteration == True:
+            printStatistics(zoobList,i,100) #prints first untouched array before anything happens
+            firstIteration = False
+            i = 1
+        #looks for food
+        zoobList = lookForFood(zoobList,envFood)
+        #looks for mates, steals food, prioritizes the starving/nonstarving, and/or has children
+        zoobList,babyList = lookForMate(zoobList)
+        #Time passes, starving or beaten culled, age increases by one
+        zoobList,survivalRate = timePass(zoobList)
+        #add babys to new generation
+        for Zoob in babyList:
+            zoobList.append(Zoob)
+        #####END OF SIM ITERATION
+        
+        #DATA: Display stats
+        #if list is empty, say that they are all dead and stop simulating
+        if len(zoobList) == 0:
+            print("They're all dead whoops")
+            i = iterations + 1
+            return
+        printStatistics(zoobList,i,survivalRate)#prints data after iteration
+    
+    writeStatistics(ws, iterations)
+    ws.write(0, 14, "Iterations")
+    ws.write(1, 14, iterations)
+    
+    ws.write(0, 15, "Initial Population")
+    ws.write(1, 15, zoobNumber)
+    
+    ws.write(0, 16, "Food Available")
+    ws.write(1, 16, envFood)
+    wb.save('SimulationStats.xls')
+
+def writeStatistics(ws, iterations):
+    ws.write(0, 0, "Iteration")
+    for i in range(iterations):
+        ws.write(i+1, 0, i)
+    writeRow(ws,1,"Population",fileNumAlive)
+    writeRow(ws,2,"Altruistic",fileAltOne)
+    writeRow(ws,3,"Indifferent",fileAltTwo)
+    writeRow(ws,4,"Selfish",fileAltThree)
+    writeRow(ws,5,"Median Selfishness",fileMedianAlt)
+    writeRow(ws,6,"Social",fileAvgSocial)
+    writeRow(ws,7,"Speed",fileAvgSpeed)
+    writeRow(ws,8,"Size",fileAvgSize)
+    writeRow(ws,9,"Vision",fileAvgVision)
+    writeRow(ws,10,"Agression",fileAvgAggr)
+    writeRow(ws,11,"Food Required",fileAvgFoodReq)
+    writeRow(ws,12,"Time Alive",fileAvgTimeAlive)
+    
+def writeRow(ws, colNumber, colName, arrayPassed):
+    ws.write(0, colNumber, colName)
+    for i in range(len(arrayPassed)):
+        ws.write(i+1, colNumber, arrayPassed[i])
+        #prints all the zoobs out from zoobList 
+def printAllZoobs(zoobList):
+    for Zoob in zoobList:
+        Zoob.printStats()
+    #writes the averages of each iteration into an excel file   
+#prints out relevent statistics of list of zoobs, i = iteration number, also puts statistics into global arrays to be printed
+def printStatistics(zoobList,i,survivalRate):
+    #Variables (all are counters to find statistics, except for number alive)
+    numberAlive = len(zoobList)
+    altOneCount = 0
+    altTwoCount = 0
+    altThreeCount = 0
+    medianAltruism = 0
+    avgSocial = 0
+    avgSpeed = 0
+    avgSize = 0
+    avgVision = 0
+    avgAggression = 0
+    avgFoodRequired = 0
+    avgTimeAlive = 0
+    
+    for Zoob in zoobList:#goes thru list to add everything
+        if Zoob.altruism == 1:
+            altOneCount = altOneCount + 1
+        elif Zoob.altruism == 2:
+            altTwoCount = altTwoCount + 1
+        else:
+            altThreeCount = altThreeCount + 1
+        
+        avgSocial = avgSocial + Zoob.socialSkills
+        avgSpeed = avgSpeed + Zoob.speed
+        avgSize = avgSize + Zoob.size
+        avgVision = avgVision + Zoob.vision
+        avgAggression = avgAggression + Zoob.aggression
+        avgFoodRequired = avgFoodRequired + Zoob.foodRequired
+        avgTimeAlive = avgTimeAlive + Zoob.timeAlive
+    
+    #finds averages for everything
+    avgSocial = avgSocial/numberAlive
+    avgSpeed = avgSpeed/numberAlive
+    avgSize = avgSize/numberAlive
+    avgVision = avgVision/numberAlive
+    avgAggression = avgAggression/numberAlive
+    avgFoodRequired = avgFoodRequired/numberAlive
+    avgTimeAlive = avgTimeAlive/numberAlive
+    if altOneCount >= altTwoCount:
+        if altThreeCount > altOneCount:
+            medianAltruism = 3
+        else:
+            medianAltruism = 1
+    else:
+        if altThreeCount > altTwoCount:
+            medianAltruism = 3
+        else:
+            medianAltruism = 2
+    #adds everything to the global arrays to be printed to a file after
+    fileNumAlive.append(numberAlive)
+    fileAltOne.append(altOneCount)
+    fileAltTwo.append(altTwoCount)
+    fileAltThree.append(altThreeCount)
+    fileMedianAlt.append(medianAltruism)
+    fileAvgSocial.append(avgSocial)
+    fileAvgSpeed.append(avgSpeed)
+    fileAvgSize.append(avgSize)
+    fileAvgVision.append(avgVision)
+    fileAvgAggr.append(avgAggression)
+    fileAvgFoodReq.append(avgFoodRequired)
+    fileAvgTimeAlive.append(avgTimeAlive)
+    
+    #prints the results
+    print("Iteration: ",i,"(Number Alive): ",numberAlive,"(AvgSocial): ",round(avgSocial,3),"(AvgSpeed): ",round(avgSpeed,3),"(AvgSize): ",round(avgSize,3),"(AvgVision): ",round(avgVision,3),"(AvgAggression): ",round(avgAggression,3),"(AvgFoodRequired): ",round(avgFoodRequired,3),"(AvgTimeAlive): ",round(avgTimeAlive,3),"(Survival Rate): ",round(survivalRate,3),"%","Altruistic: ",altOneCount,"Indifferent: ",altTwoCount,"Selfish: ",altThreeCount)
+
+#generates an array of zoobs of size zoobNumber. If you want them to be identical, enter a sharedAncestor
+def generateZoobArray(zoobNumber,sharedAncestor):
+    print("Generating initial population...")
+    zoobList = []
+    if sharedAncestor:
+        ancestor = Zoob()
+        #ancestor.printStats()##DEBUG
+        for i in range(zoobNumber):
+            newBabyZoobie = ancestor.copyZoob()
+            zoobList.append(newBabyZoobie)
+    else:
+        for i in range(zoobNumber):
+            newBabyZoobie = Zoob()
+            #newBabyZoobie.printStats()##DEBUG
+            zoobList.append(newBabyZoobie)
+    print("Done generation initial population...")
+    return zoobList
+    
+#has zoobList look for food in an environment with envFood amount of food
+def lookForFood(zoobList,envFood):
+    #Sort array in ascending order by speed
+    zoobList.sort()
+    #goes through the list looking for food
+    for Zoob in zoobList:
+        if envFood == 0:#no more food, stop letting zoobs look
+            return zoobList
+        else:
+            isLooking = True#stops looking once it has enough food or cant find any, having enough is any value that means its not starving
+            roundOfSearching = 0
+            while isLooking:
+                #if not Zoob.isStarving:
+                #   print(Zoob.food,'/',Zoob.foodRequired)
+                #  isLooking = False#done looking for food
+                chance = random.randint(1,100)+(findFoodChallenge*roundOfSearching)#     if vision greater, then Zoob finds food, gets harder 
+                if Zoob.vision > chance:#      --> zoob is getting munchies today baby
+                    foodCarryCap = random.randint(int((Zoob.size+Zoob.speed)/2),int(Zoob.size+Zoob.speed))/100#   zoob can carry its size to max size ratio in food (largest can carry 1.0 food), smallest can carry .01 food #MAKE THIS A VARIABLE RANGE
+                    if (envFood - foodCarryCap) > 0:#   enough food to be taken
+                        Zoob.addFood(foodCarryCap*2) 
+                        envFood= envFood-foodCarryCap
+                    else:#could still carry more food
+                        Zoob.addFood(envFood)
+                        envFood=0
+                        isLooking = False#no more food
+                else:
+                    isLooking = False#failed to find more food(thinks no more food is out there)
+                roundOfSearching = roundOfSearching + 1
+    #Has updated all Zoobs in Zoob list with their food
+    return zoobList
+
+#has the zoobs in zoobList look for a mate
+def lookForMate(zoobList):
+    newZoobGeneration=[]#new gen of Zoobs
+    if zoobList == None:
+        print("somethings wrong! got passed an embty zooblist at line 64 of SimDriver:lookForMate")#error print
+    for Zoob in zoobList:
+        babyZoob = Zoob.mate(zoobList)#returns baby Zoob if created, Nonetype object if failure to mate
+        if babyZoob != None:
+            newZoobGeneration.append(babyZoob)#adds new bby Zoob to new generation(cannot be mated with yet)
+    return zoobList,newZoobGeneration#returns zoobs and baby zoobs in a seperate array, to be only added once the iteration ends(aging is called)
+
+#signals a new iteration of the simulation and ages the zoobs who survive, removes dead zoobs from array
+def timePass(zoobList):
+    #Death counter used to find survival rate over each iteration
+    deathCounter = 0
+    initialPopulation = len(zoobList)
+    newZoobs = zoobList.copy()
+    for i in reversed(range(len(zoobList))):#starts from back as to not miss index(when deleted)
+        currentZoob = zoobList[i]
+        if currentZoob.age() == False:#if Zoob dies
+            deathCounter = deathCounter + 1
+            newZoobs.pop(i)
+        else: #zoob dont die   
+            newZoobs[i].age()
+    stillAlive = initialPopulation-deathCounter
+    survivalRate = (stillAlive/initialPopulation) * 100
+    return newZoobs,survivalRate
+
+if __name__ == "__main__":
+    main()
---
+++ Zoob.py
@@ -0,0 +1,224 @@
+import random #importing Math class
+
+
+mutationRate = 50
+mutationIntensity = 30
+class Zoob:
+	#Traits of a Zoob
+	#//Food Rules
+	#// Food < .5 --> chance of survival, no reproduction
+	#// .5 <= Food < 1 ---> Survival, chance of reproduction if > .5
+	#// Food >=1 ---> Reproduction, chance of multiple offspring
+    #Enviornmentvariable
+    #//Traits
+	#altruism#//1 =  altruistic, looks to give food to other zoobs before mating, 2 = indifferent, searches whole pool for mates and doesnt donate, 3 = searches through pool of people with enough food to survive
+	#socialSkills# // 1-100 chance that this creature will fuck another creature and attempt to reproduce. say if you have a low chance you may go thru array and not find a parter becuase your social skills too weak
+	#speed# //1-100 speed of a zoob, high speed goes out to find food before slower zoobs and has a higher chance (more food needed for higher speed)
+	#size #//1-100 size of a zoob, more food needed the bigger it is, also higher chance of winning fights
+	#vision# //1-100 higher chance to find food 
+	#aggression#// 1-100 chance of fighting if mate is not suitable
+	#//Zoob Metrics
+	#timeAlive #//how many turns the zoob has been alive
+	#food #//creatures who dont find a mate save their excess food
+	#foodRequired #// amoutn of food needed to survive, determine at birth by traits #randominit
+    def __init__(self, parentA = None, parentB = None):
+        if parentA == None:
+            #no parents passed, created from nothing
+            self.altruism = random.randint(1,3)
+            #  random.randint(1,3)
+            self.socialSkills = random.randint(1,100)
+            self.speed = random.randint(1,100)
+            self.size = random.randint(1,100)
+            self.vision = random.randint(1,100)
+            self.aggression = random.randint(1,100)
+            #self.libido = randint(1,10)######################NEWEST FEATUREEEEEE: HOW MANY TIMES THEY TRY TO MATE, WILL ALSO TAKE PARENTS GENE AND MUTATE, SHOULD ALSO MAKE IT LESS LIKELY TO BEAT UP ANOTHER ZOOB, THEY LOWKEY MERCIN EACH OTHER
+            #//Metrics
+            self.timeAlive = 0
+            self.food = 0.0
+            self.foodRequired = (self.speed+self.size)/100#//to survive
+            self.isBeaten = False
+        else:##Is child
+            #//Constructor with two parent args
+            #Mutation threshold
+            if random.randint(1,100) < mutationRate:
+                if parentA.altruism + parentB.altruism <= 3:
+                    self.altruism = random.choice([1,2])
+                else:
+                    self.altruism = random.choice([2,3])
+            else: 
+                self.altruism = random.choice([parentA.altruism,parentB.altruism])
+            self.socialSkills = self.mixTrait(parentA.socialSkills,parentB.socialSkills)
+            self.speed = self.mixTrait(parentA.speed,parentB.speed)
+            self.size = self.mixTrait(parentA.size,parentB.size)
+            self.vision = self.mixTrait(parentA.vision,parentB.vision)
+            self.aggression = self.mixTrait(parentA.aggression,parentB.aggression)
+            #//Metrics
+            self.timeAlive = 0
+            self.food = 0.0
+            self.foodRequired=(self.speed+self.size)/100#//to survive not  
+            self.isBeaten = False
+            #//end constructor
+    def isStarving(self):
+        if self.food >= self.foodRequired: #has enough food to survive or more
+            #print("Not Starving","-Food:",self.food," -Required:",self.foodRequired)
+            return False
+        else: #doesnt have enough food to survive
+            #print("Died of starving","-Food:",self.food," -Required:",self.foodRequired)
+            return True
+    ####Every iteration of the sim, if isstarving and some threshold of having a kid is satisfied run mate on all zoob in lists
+    def addFood(self, foodAdded):
+        self.food = self.food + foodAdded
+        return
+    def age(self):
+        if self.isStarving(): #dies from starvation
+            #print("Died of starvingAGE","-Food:",self.food," -Required:",self.foodRequired," -isStarving:",self.isStarving())
+            return False#die
+        elif self.isBeaten: #dies from beaten
+            #print("died of getting kicked")
+            return False#die
+        else: #lives
+            #print("Not StarvingAGE","-Food:",self.food," -Required:",self.foodRequired," -isStarving:",self.isStarving())
+            self.food = self.food - self.foodRequired #Eat
+            self.timeAlive = self.timeAlive + 1 #Age
+            return True#survive
+    def mate(self, zoobList):#Zoob[] //mating loop
+        #asks if zoob is starving. if it is, it doesn't look for a mate
+        if self.isStarving():
+            return None #returns false when starving, no baby
+        
+        foundMate = False
+        #theMate#Zoob
+        if (self.altruism == 1):#//prefer those that dont have enough
+            for zoob in zoobList:
+                if not foundMate:
+                    if zoob.isStarving() and (self.food > self.foodRequired):#//prefers to share with starving, can only share if it has enough to share
+                        if self.tryMate(zoob):#//tests compatability
+                            foundMate = True
+                            theMate = zoob#//stores mate
+                            break
+            if not foundMate:#//give up on finding a starving mate
+                for zoob in zoobList:
+                    if not zoob.isStarving():
+                        if self.tryMate(zoob):#//tests compatability
+                            foundMate = True
+                            theMate = zoob#//stores mate
+                            break
+        elif self.altruism == 3:#//prefers those that have enough food
+            for zoob in zoobList:
+                if not foundMate:
+                    if not zoob.isStarving():#//prefers non starving
+                        if self.tryMate(zoob):#//tests compatability
+                            foundMate = True
+                            theMate = zoob#//stores mate
+                            break#//leave for
+            if not foundMate:#//give up on finding a healthy mate
+                for zoob in zoobList:#//looks for any mate
+                    if zoob.isStarving() and (self.food > self.foodRequired): #decides to mate with starving one, only if it has enough food to share:
+                        if self.tryMate(zoob):#//tests compatability
+                            foundMate = True
+                            theMate = zoob#//stores mate
+                            break#//    leave for
+        else:#//    doesnt care
+            for zoob in zoobList:
+                if not foundMate:
+                    if self.tryMate(zoob):#//tests compatability
+                        if not zoob.isStarving(): #finds a mate as long as it is not starving, or if it is starving and we have extra to give. prevents something with no excess mating with starving creature
+                            foundMate = True
+                            theMate = zoob#//stores mate
+                            break#//    leave for
+                        elif zoob.isStarving() and (self.food > self.foodRequired):
+                            foundMate = True
+                            theMate = zoob#//stores mate
+                            break#//    leave for
+        if foundMate:#//            found mate in whole array?
+            return self.haveChild(theMate)#//   try to have child(ren)
+        return None
+    def tryMate(self, potentialMate):#  //check compatability of mates
+        liklihood = int((self.socialSkills + potentialMate.socialSkills)/2)#//the avg btwn range 1-x
+        test = random.randint(1, 100)#  //the actualy case in which they mate
+        if test <= liklihood:#//        if case between [1:x], sucess. If case between (x:100], fail.
+            #print("maybe bbaby")
+            return True
+        #A Challenger approaches...
+        #if random.randint(1, 100) <= self.aggression:
+            ##self.takeFood(potentialMate)#//If zoob that failed to mate is aggresive than fight bad mate
+            ##print("no bab + punch")
+        return False
+    def takeFood(self, failedMate):#        //steal food of failed mate depending on aggression
+        if self.size+self.speed >= failedMate.size+failedMate.speed:#//clash between zoob
+            self.food = self.food + failedMate.food#  //adds their food to their pile
+            failedMate.food = 0#            //leaves them nothing
+        else:
+            failedMate.food = failedMate.food + self.food#//adds the host Zoobs food to their pile
+            failedMate.food = 0#           //leaves the host Zoob nothing
+        return#///potentially kill them in which case they get deleted from array   MUST RETURN FALSE OR SOME KILL VALUE ######## IF KILLED CHANGE IS BEATEN BOOLEN TO TRUE WHICH IS EVALUATED ON AGING PROCESS(dies)
+    def haveChild(self, mate):
+        selfExcess = 0 #self excess food --- self is at least not starving becuase of if statement at top of mate
+        mateExcess = 0  #mates excess food --- mate could be starving, is likely for alturistic zoobs searching
+        excessFood = 0  #total partnership excess food
+        if mate.isStarving(): #the zoob that has been chosen to mate with is starving. give it food
+            selfExcess = self.food - self.foodRequired #finds excess of self
+            self.food = self.food - selfExcess #removes excess from self.food
+            mate.food = mate.food + selfExcess #gives the excess to the mate
+            selfExcess = 0 #resets selfExcess since it is empty now
+            mateExcess = mate.food - mate.foodRequired #find the excess of the mate, now that the self has donates all excess to it
+            if mateExcess > 0: #mate has excess. there is excess in the partership. may try for a baby
+                excessFood = mateExcess #sets the excess of the partnership, doesnt add selfExcess because it is empty
+                mate.food = mate.food - mateExcess #removes the excess from the mate. now both foods are updated, and the food to make a baby is stored in excessFood
+            else: #after donation by self, mate is still starving. set all excess to 0. no baby. self simply donated to mate
+                excessFood = 0
+                mateExcess = 0
+        else: #mate is not starving. this means that both parties at least have enough food to survive.
+            selfExcess = self.food - self.foodRequired #finds excess in self, has a minimum of 0
+            mateExcess = mate.food - mate.foodRequired #finds excess in mate, minimum of 0
+            excessFood = selfExcess + mateExcess #finds total excess in partnership
+            if excessFood > 0: #if there is any excess food in the partnership to try for a kid
+                self.food = self.food - selfExcess #remove self excess from selffood
+                mate.food = mate.food - mateExcess #remove mate exces from matefood
+
+        if excessFood > 0.0:#//if surplus of food is great enough, attempt child
+            chance = int(excessFood*50)#casts excess food to int, having twice the food required(or greater) means 100% chance to reproduce:
+            if chance > random.randint(1,int((self.foodRequired + mate.foodRequired)*50)):
+                if excessFood > (self.foodRequired + mate.foodRequired): #there is enough leftover food to give each a days worth of extra
+                    self.food = self.food + self.foodRequired #gives self an extra day of food
+                    mate.food = mate.food + mate.foodRequired #gives mate an extra day of food
+                    excessFood = excessFood - self.foodRequired - mate.foodRequired #subrtacts the removals from excessFood
+                    self.food = self.food + excessFood #gives remaining excess to self
+                else: #not enough, randomly splits food up between the two
+                    foodPortion = random.randint(1,int(excessFood*100))
+                    self.addFood(foodPortion/100)
+                    mate.addFood((excessFood-foodPortion)/100)
+                #print("had Babb")
+                return self.createChild(mate)#creates child and returns it to be passed up via the mate call
+            #//if sucess
+            ##//create child with genevariance specified by enviornment, run createChild(), append to list
+            return None
+    def createChild(self, mate):
+        return Zoob(parentA=self,parentB=mate)
+    def mixTrait(self, parentATrait, parentBTrait):
+        mixGene=(parentATrait + parentBTrait)/2
+        if random.randint(1, 100) < mutationRate:#will Mutate, mutation rate is a threshold between 1,100 if 1 no mutation, if 100 always mutate
+            mixGene = mixGene+random.randint(-1*mutationIntensity, mutationIntensity)#mutated gene
+            if mixGene>100:
+                mixGene = 100###No upper bound if toggled
+            if mixGene<1:
+                mixGene = 1
+        return mixGene
+    def __lt__(self, other):
+        return self.speed > other.speed#.sort will sort based on speed
+    def printStats(self):
+        print('Altruism:',str(self.altruism),' Social:',str(self.socialSkills),' Speed',str(self.speed),' Size:',str(self.size),'Vision:',str(self.vision),'Agression:',str(self.aggression),' Time Alive:',str(self.timeAlive),' Food:',str(self.food),' Food Required:',str(self.foodRequired))
+        return
+    def copyZoob(self):
+        copy = Zoob()
+        copy.altruism = self.altruism
+        copy.socialSkills = self.socialSkills
+        copy.speed = self.speed
+        copy.size = self.size
+        copy.vision = self.vision
+        copy.aggression = self.aggression
+        copy.foodRequired = self.foodRequired
+        return copy
+        
+    def __copy__(self):
+        return self.copyZoob()
